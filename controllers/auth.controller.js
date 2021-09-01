3620ee416a963f6c00dc530d5fa1103deb73320e
odonML <odon_ml@outlook.com>
2021-09-01 11:54:31 -0500

    documentacion terminada

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,43 +1,42 @@
-const Usuario = require('../models/usuario.models');
-const bcrypt = require("bcryptjs");
-const { generarJWT } = require('../helpers/jwt.helpers');
+const Usuario = require('../models/usuario.models'); //Importamos el modelo
+const bcrypt = require("bcryptjs"); //Importamos libreria para encriptar password
+const { generarJWT } = require('../helpers/jwt.helpers'); //Importamos la funcion que genera el JWT
 
-const login = async (req, res) => {
-    const {email, password} = req.body;
-    try{
-        const usuarioDB = await Usuario.findOne({ email });
-        if(!usuarioDB){
-            res.status(404).json({
+const login = async (req, res) => { //Funcion para hacer login
+    const {email, password} = req.body; //Desestructuramos el email y el password del cuerpo de la peticion
+    try{//En caso que la promesa se cumpla
+        const usuarioDB = await Usuario.findOne({ email });//Verificamos que exista un usuario con ese email
+        if(!usuarioDB){//Validacion si el usuario con ese email no existe
+            res.status(404).json({//Respuesta con estatus 404
                 ok: false,
-                msj: 'Email no encontrado'
+                msj: 'Email no encontrado'//Mensaje de la respuesta
             })
         }
 
-        const validPassword = bcrypt.compareSync(password, usuarioDB.password);
-        if(!validPassword){
-            res.status(400).json({
+        const validPassword = bcrypt.compareSync(password, usuarioDB.password);//Verificacion de el password encriptado
+        if(!validPassword){ //Validacion en base a la comparacion
+            res.status(400).json({//Respuesta con estatus 400
                 ok: false,
-                msj: 'Password no es valido'
+                msj: 'Password no es valido'//Mensaje de la respuesta
             });
         }
 
         // generar JWT
-        const token = await generarJWT(usuarioDB.id)
-
-        res.json({
+        const token = await generarJWT(usuarioDB.id);//Funcion que genera el JWT
+        res.json({//Respuesta con el JWT generado
             ok: true,
-            token
+            token // JWT
         })
 
-    }catch(err){
-        console.log(err);
-        res.status(500).json({
+    }catch(err){//En caso que la promesa falle
+        console.log(err);//Impresion en consola del error
+        res.status(500).json({//Respuesta con estatus 500
             ok: false,
-            msj: 'ocurrio un error en el login'
+            msj: 'ocurrio un error en el login'//Mensaje de la respuesta
         });
     }
 }
 
-module.exports = {
+module.exports = { //Exportamos la funcion de login
     login
 }
